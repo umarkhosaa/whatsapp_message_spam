b479eb78b4e819bbbe70b48b8cb32b5e4827eb85
umarkhosaa <84055078+umarkhosaa@users.noreply.github.com>
2021-05-24 19:43:24 +0500

    Update whatsapp_message_spam.py

--- whatsapp_message_spam.py
+++ whatsapp_message_spam.py
@@ -5,10 +5,10 @@ import time
 # To Make sure the program runs perfectly you need to:
 # i) Have logged in to WhatsApp earlier on the same device through Chrome
 # ii) Chrome is open in Background
-# iii) As WhatsApp loads click on the profile you wish to send the message and wait 20 seconds
+# iii) As WhatsApp loads click on the profile you wish to send the message and wait 30 seconds
 # iv) Make sure Caps Lock isn't ON
-n = int(input("Enter the number of times you wish to print:"))
-text = input("Enter the text you wish to send:")
+n = int(input("Enter the number of times you wish to print: "))
+text = input("Enter the text you wish to send: ")
 wb.open("https://web.whatsapp.com/")
 time.sleep(30)
 for j in range(n):
